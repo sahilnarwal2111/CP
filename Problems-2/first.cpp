f5d5acc02c076b4006231433ce57e8cd7e9d0d8f
sahilnarwal2111 <500097751@stu.upes.ac.in>
2024-01-31 09:41:51 +0530

    jan 31

--- Problems-2/first.cpp
+++ Problems-2/first.cpp
@@ -1,51 +1,88 @@
+// #include<iostream>
+// #include<vector>
+// #include<string>
+// #include<algorithm>
+// #include<cmath>
+// #include<set>
+// #include <map>
+// #include <queue>
+// #include <stack>
+// #include <cstdlib>
+// #define mod (ll)(1e9+7)
+// #define int long long
+// #define endl '\n'
+// using namespace std;
+// void solve(){
+//     int n, m; cin>>n>>m;
+//     vector<int> arr(n,0) ;
+//     for(int i = 0 ; i < n ; i++) cin>>arr[i];
+
+//     for(int i = 0 ; i < arr.size(); i++){
+//         if(arr[i] < m){
+//             if(i + 1 < arr.size()){
+//                 arr[i+1]+=arr[i];
+//                 arr.erase(arr.begin() + i);
+//                 i--; 
+//             }
+//         }
+//     }
+//     while(arr.size()>1 && arr[arr.size()-1] < m){
+//         arr[arr.size()-2]+=arr[arr.size()-1];
+//         arr.pop_back();
+//     }
+//     cout<<arr.size()<<endl;
+// }
+// signed main(){
+//     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+//     int t; cin>>t;
+//     while(t--){
+//         solve();
+//     }
+//     return 0;
+// }
+
 #include<iostream>
 #include<vector>
-#include<string>
-#include<algorithm>
-#include<cmath>
-#include<set>
-#include <map>
-#include <queue>
-#include <stack>
-#include <cstdlib>
-#define mod (ll)(1e9+7)
 #define int long long
-#define endl '\n'
 using namespace std;
 
-bool vectSort(const vector<int>& v1, const vector<int>& v2){
-    return v1[1] < v2[1];
+void solve(){
+    int n, m; 
+    cin >> n >> m;
+    vector<int> arr(n, 0);
+
+    for(int i = 0 ; i < n ; i++) 
+        cin >> arr[i];
+
+    int result = 0;
+    int currentSum = 0;
+
+    for(int i = 0; i < n; i++) {
+        if(arr[i] >= m) {
+            result++;
+            continue;
+        }
+        currentSum = arr[i];
+        int j = i + 1;
+        while(j < n && currentSum < m) {
+            currentSum += arr[j++];
+        }
+        if(currentSum >= m) {
+            result++;
+            i = j - 1;
+        }
+    }
+
+    cout << result << endl;
 }
+
 signed main(){
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     
-    int t; cin>>t;
+    int t; 
+    cin >> t;
     while(t--){
-        int n, p; cin>>n>>p;
-        vector<vector<int> > arr(n,vector<int>(2,0));
-        for(int i = 0 ; i < n ; i++){
-            cin>>arr[i][0];
-        }
-        for(int i = 0 ; i < n ; i++){
-            cin>>arr[i][1];
-        }
-        sort(arr.begin(), arr.end(), vectSort); 
-        int cost = p;
-        int itr = 0;
-        for(int persons = 2; persons <= n ; persons++){
-            if(arr[itr][0]>0){
-                if(arr[itr][1]<p){
-                    cost+=arr[itr][1];
-                    arr[itr][0]--;
-                }else{
-                    cost+=p;
-                }
-            }else {
-                itr++;
-                persons--;
-            }
-        }
-        cout<<cost<<endl;       
+        solve();
     }
 
     return 0;
--- Problems-2/jonny_and_his_hobbies.cpp
+++ Problems-2/jonny_and_his_hobbies.cpp
@@ -4,28 +4,62 @@
 #include<algorithm>
 #include<cmath>
 #include<set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <cstdlib>
+#include <climits>
+#define mod (ll)(1e9+7)
+#define ll long long
 using namespace std;
+
+vector<vector<int> > dp(mod + 1, vector<int>(mod + 1, -1));
+
+void rec(int a, int b, int i , int *res){
+    if( a < 1 || b < 1 || a > mod || b > mod){
+        return;
+    }
+    if( a == b){
+        *res = min(*res, i-1);
+        dp[a][b] = *res;
+        return;
+    }
+    if(dp[a][b]==-1) {
+        if(i&1){ 
+            rec(a + i , b, i + 1, res);
+            rec(a, b + i , i + 1, res);
+        }
+        else
+        { 
+            if (a >= i) 
+                rec(a - i, b, i + 1, res);
+            
+            if (b >= i) 
+                rec(a, b - i, i + 1, res);
+        }
+    }
+
+}
+
+void solve(){
+    int res = INT_MAX;
+    int a,b; cin>>a>>b;
+
+    if(a == b) {
+        cout<<0<<endl;
+        return;
+    }
+
+    rec(a,b,1, &res);
+    cout<<dp[a][b]<<endl;
+
+}
+
 int main(){
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     int t; cin>>t;
     while(t--){
-        int n; cin>>n;
-        set<int> arr;
-        for(int i = 0 ; i < n ; i++) {
-            int temp ; cin>>temp;
-            arr.insert(temp);
-        }
-        set<int> temp;
-        int ans = -1;
-        for(int i = 1 ; i < 1024; i++){
-            for(auto &el : arr){
-                temp.insert(el ^ i);
-            }
-            if(temp == arr){
-                ans = i;
-                break;
-            }
-        }
-        cout<<ans<<endl;
+        solve();
     }
     return 0;
 }
