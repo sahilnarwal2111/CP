4d73d1996ccdd25ab801ba8d68315f7cdc31c991
sahilnarwal2111 <500097751@stu.upes.ac.in>
2024-04-17 19:59:59 +0530

    Hello Wolrd!

--- Problems-2/first.cpp
+++ Problems-2/first.cpp
@@ -10,35 +10,16 @@
 #include <unordered_map>
 #include <stack>
 #include <cstdlib>
+#include <numeric>
+
 #define mod (ll)(1e9+7)
 #define int long long
 #define ll long long
 #define endl '\n'
 using namespace std;
 
-void solve() {  
-  int n ; cin>>n;
-  int sum ; cin>>sum;
-  if(sum % 2 == 0){
-    int arr[n];
-    int q = sum/n;
-    for(int i = 0; i < n ; i++) arr[i] =q ;
-    int r = sum % n;
-    if(r % 2 == 0){
-      for(int i = 0 ; i < r ; i++){
-        arr[i]++;
-      }
-      for(int i = 0 ; i < n ; i++) cout<<arr[i]<<" ";
-      cout<<endl;
-    }
-    else{
-      cout<<-1<<endl;
-      return;
-    } 
-  }
-  else{
-    cout<<-1<<endl;
-  }
+void solve() {   
+  
 }
 signed main(){
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
