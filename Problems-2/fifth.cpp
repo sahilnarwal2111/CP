e998045c61a7b1eba21cb4da0d2bf7cd9b83b618
sahilnarwal2111 <500097751@stu.upes.ac.in>
2024-04-17 22:39:34 +0530

    Codechef Starters 130 Div 3

--- Problems-2/fifth.cpp
+++ Problems-2/fifth.cpp
@@ -7,54 +7,88 @@
 #include <map>
 #include <queue>
 #include <unordered_set>
-
+#include <unordered_map>
 #include <stack>
 #include <cstdlib>
+#include <numeric>
+#include <climits>
 #define mod (ll)(1e9+7)
 #define int long long
+#define ll long long
 #define endl '\n'
 using namespace std;
 
-
-void solve(){
+void solve() {   
     int n; cin>>n;
-    int a, b ; cin>>a>>b;
-    if(a == 1 && b == 1){
-        if( n & 1){
-            cout<<"Alice"<<endl;
+    string str ; cin>>str;
+    int start = -1;
+    for(int i = 0 ; i < n; i+=2){
+        if(str[i]!= str[i+1]){
+            start = i;
+            break;
         }
-        else{
-            cout<<"Bob"<<endl;
+    }
+    int end = -1;
+    for(int i = n - 1; i > 0 ; i = i - 2){
+        if(str[i]!= str[i-1]){
+            end = i;
+            break;
         }
+        
+    }
+    if(start == -1 || end == -1){
+        cout<<0<<endl;
         return;
     }
-    if(a >= n){
-        cout<<"Alice"<<endl;
+    if(end == start + 1){
+        cout<<1<<endl;
+        cout<<start + 1<<endl;
         return;
     }
-
-    if(n % (a + n) == 0){
-        cout<<"Bob"<<endl;
-        return;
+    int mid = (start + end) / 2;
+    
+    string s1 = "", s2 ="";
+    for(int i =start ; i <= mid ; i++){
+        s1 += str[i];
     }
-    int remain = n % ( a + b );
-
-    if(remain <= a ){
-        cout<<"Alice"<<endl;
-        return;
+    for(int i = mid + 1; i <= end; i++){
+        s2 += str[i];
     }
-    cout<<"Bob"<<endl;
-
+    bool flag = true;
+    int size = s1.size();
+    if(s1[0] == s1[size-1]){
+        for(int i = 0 ; i < size; i++){
+            if(s1[i] != s2[size-1-i]){
+                flag = false;
+                break;
+            }
+        }
+    }
+    else if(s1[0] == s2[0]){
+        for(int i = 0 ; i < size; i++){
+            if(s1[i] != s2[i]){
+                flag = false;
+                break;
+            }
+        }
+    }
+    if(flag){
+        cout<<size<<endl;
+        for(int i = 0; i < 2 * size ;i+=2){
+            cout<<(i+start + 1)<<" ";
+        }
+        cout<<endl;
+    }
+    else{
+        cout<<-1<<endl;
+    }
 
 }
-
-
 signed main(){
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     int t; cin>>t;
     while(t--){
         solve();
     }
-
+    return 0;
 }
-
--- Problems-2/first.cpp
+++ Problems-2/first.cpp
@@ -11,7 +11,7 @@
 #include <stack>
 #include <cstdlib>
 #include <numeric>
-
+#include <climits>
 #define mod (ll)(1e9+7)
 #define int long long
 #define ll long long
@@ -19,7 +19,47 @@
 using namespace std;
 
 void solve() {   
-  
+    int n; cin>>n;
+    int arr[n]; for(int i =0 ; i < n ; i++) cin>>arr[i];
+    int minSoFar = INT_MAX;
+    int minEndingHere = 0;
+    int start =0, end = 0, s =0;
+    for(int i = 0 ; i < n ; i++){
+        minEndingHere += arr[i];
+        // minSoFar = min(minSoFar, minEndingHere);
+        if(minSoFar > minEndingHere){
+            minSoFar = minEndingHere;
+            start = s;
+            end = i;
+        }
+        if(minEndingHere > 0){
+            minEndingHere = 0;
+            s = i + 1;
+        }
+    }
+    int total = 0;
+    for(int i = 0 ; i < n ; i++) total += arr[i];
+    if(n == 2){
+        cout<<abs(total)<<endl;
+        return;
+    }
+    if(start != end){
+        int sum = 0;
+        for(int i = start ; i <= end; i++){
+            sum +=arr[i];
+        }
+        sum = sum * 2;
+        sum *= -1;
+        total = total + sum;
+        cout<<total<<endl;
+        return;
+    }
+    else{
+        // cout<<abs(total)<<endl;
+        
+
+    }
+
 }
 signed main(){
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
