9ac63042f25bdae051f45c72c58e2ecaafae90c3
sahilnarwal2111 <500097751@stu.upes.ac.in>
2024-05-05 19:24:09 +0530

    day - 3

--- Problems-2/first.cpp
+++ Problems-2/first.cpp
@@ -13,37 +13,27 @@
 #include <numeric>
 #include <climits>
 #define mod (ll)(1e9+7)
-// #define int long long
+#define int long long
 #define ll long long
 #define endl '\n'
 using namespace std;
 
 void solve() {   
-    int n, k;
-    cin>>n>>k;
-    string str; cin>>str;
-    int f[26];
-    for(int i = 0 ; i < 26 ; i++) f[i]  = 0;
-    for(int i = 0 ; i < n ; i++){
-        int idx = str[i] - 'a';
-        f[idx]++;
+    int n, k; cin>>n>>k;
+    int arr[n]; for(int i = 0 ; i < n ; i++) cin>>arr[i];
+    sort(arr, arr + n);
+    int ans = 0;
+    int cnt =0;
+    for(int i = 1; i < n ; i++){
+        if(arr[i] - arr[i-1] <= k){
+            cnt++;
+            ans = max(cnt, ans);
+        }
+        else{
+            cnt = 0;
+        }
     }
-    int odd = 0;
-    int even = 0;
-    for(int i = 0 ; i < 26 ; i++){
-        if(f[i] & 1) odd++;
-        else even++;
-    }
-    if(odd == 1) {
-        cout<<"YES"<<endl;
-        return ;
-    }
-    if(odd - 1 > k ){
-        cout<<"NO"<<endl;
-        return;
-    }
-    cout<<"YES"<<endl;
-
+    cout<<n - ans - 1<<endl;
 }
 signed main(){
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
@@ -53,115 +43,3 @@ signed main(){
     }
     return 0;
 }
-
-
-    // int v1, v2;
-    // int ans = 0;
-    // // up a //left b
-    // v1 = y1 + a;
-    // v2 = x1 - b;
-    // //check
-    //     cout<<v1<<" "<<v2<<endl;
-    // if(v2 - b == x2 && v1 - a == y2) {
-    //     ans++;
-    // }
-
-    // //up a // right b
-    // v1 = y1 + a;
-    // v2 = x1 + b;
-    //     cout<<v1<<" "<<v2<<endl;
-    // if(v2 + b == x2 && v1 - a == y2) {
-    //     ans++;
-    // }
-
-    // // left a // up b
-    // v1 = y1 + b;
-    // v2 = x1 + a;
-    //     cout<<v1<<" "<<v2<<endl;
-    // if(v1 + b == y2 && v2 - a == x2) {
-    //     ans++;
-    // }
-
-    // //left a // down b
-    // v1 = y1 - b;
-    // v2 = x1 + a;
-    //     cout<<v1<<" "<<v2<<endl;
-    // if(v1 - b == y2 && v2 - a == x2) {
-    //     ans++;
-    // }
-
-    // // down a right b
-    // v1 = y1 - a;
-    // v2 = x1 + b;
-    //     cout<<v1<<" "<<v2<<endl;
-    // if(v2 + b == x2 && v1 +a == y2) {
-    //     ans++;
-    // }
-
-    // //down a left b
-    // v1 = y1 - a;
-    // v2 = x1 - b;
-    //     cout<<v1<<" "<<v2<<endl;
-    // if(v2 - b == x2 && v1 + a == y2) {
-    //     ans++;
-    // }
-
-    // //left a down b
-    // v1 = y1 - b;
-    // v2 = x1 - a;
-    //     cout<<v1<<" "<<v2<<endl;
-    // if(v1 - b == y2 && v2 + a == x2) {
-    //     ans++;
-    // }
-
-    // //left a up b
-    // v1 = y1 + b;
-    // v2 = x1 - a;
-    //     cout<<v1<<" "<<v2<<endl;
-    // if(v1 + b == y2 && v2 + a == x2) {
-    //     ans++;
-    // }
-
-
-    // //     // up a //left b
-    // // v1 = y1 + b;
-    // // v2 = x1 - a;
-    // // //check
-    // // if(v2 - a == x2 && v1 - b == y2) ans++;
-
-    // // //up a // right b
-    // // v1 = y1 + b;
-    // // v2 = x1 + a;
-    // // if(v2 + a == x2 && v1 - b == y2) ans++;
-
-    // // // left a // up b
-    // // v1 = y1 + a;
-    // // v2 = x1 + b;
-    // // if(v1 + a == y2 && v2 - b == x2) ans++;
-
-    // // //left a // down b
-    // // v1 = y1 - a;
-    // // v2 = x1 + b;
-    // // if(v1 - a == y2 && v2 - b == x2) ans++;
-
-    // // // down a right b
-    // // v1 = y1 - b;
-    // // v2 = x1 + a;
-    // // if(v2 + a == x2 && v1 +b == y2) ans++;
-
-    // // //down a left b
-    // // v1 = y1 - b;
-    // // v2 = x1 - a;
-    // // if(v2 - a == x2 && v1 + b == y2) ans++;
-
-    // // //left a down b
-    // // v1 = y1 - a;
-    // // v2 = x1 - b;
-    // // if(v1 - a == y2 && v2 + b == x2) ans++;
-
-    // // //left a up b
-    // // v1 = y1 + a;
-    // // v2 = x1 - b;
-    // // if(v1 + a == y2 && v2 + b == x2) ans++;
-
-    // cout<<ans<<endl;
