d0df89f4117e4b725f22e32cd3d650f1d946e2a1
sahilnarwal2111 <500097751@stu.upes.ac.in>
2024-05-08 20:31:52 +0530

    Day - 4 !

--- Problems-2/fifth.cpp
+++ Problems-2/fifth.cpp
@@ -21,42 +21,43 @@ using namespace std;
 void solve() {   
     int n; cin>>n;
     int arr[n]; for(int i =0 ; i < n ; i++) cin>>arr[i];
-    int minSoFar = INT_MAX;
-    int minEndingHere = 0;
-    int start =0, end = 0, s =0;
+    if(n == 1) {
+        cout<<0<<endl;
+        return;
+    }
+    int maxi = arr[n-1] - arr[0];
+    for(int i = 1 ; i < n; i++){
+        maxi = max(maxi, arr[i] - arr[i-1]);
+    }
+    //second case minimuma at first and as it at last
+    int mini = INT_MAX;
+    int iMini = -1;
     for(int i = 0 ; i < n ; i++){
-        minEndingHere += arr[i];
-        // minSoFar = min(minSoFar, minEndingHere);
-        if(minSoFar > minEndingHere && s != i){
-            minSoFar = minEndingHere;
-            start = s;
-            end = i;
-        }
-        if(minEndingHere > 0){
-            minEndingHere = 0;
-            s = i + 1;
+        if(mini > arr[i]){
+            mini = arr[i];
+            iMini = i;
         }
     }
-    
-    cout<<minSoFar<<endl;
-    cout<<start<<" "<<end<<endl;
-    // int total = 0;
-    // for(int i = 0 ; i < n ; i++) total += arr[i];
-    // if(n == 2){
-    //     cout<<abs(total)<<endl;
-    //     return;
+    if(iMini != n - 1){
+        maxi = max(arr[n-1] - mini, maxi);
+    }
+    //third case maximum at last and first as it as
+    int maximum = INT_MIN;
+    int iMax = -1;
+    for(int i = 0 ; i < n ; i++){
+        if(maximum < arr[i]){
+            maximum = arr[i];
+            iMax = i;
+        }
+    }
+    if(iMax != 0){
+        maxi = max(maximum - arr[0], maxi);
+    }
+    // if(iMax == 0 && iMini == n-1){
+    //     maxi = max(arr[0]-arr[1], maxi);
+    //     maxi = max(arr[n-2] - arr[n-1], maxi);
     // }
-    // if(start != end){
-    //     int sum = 0;
-    //     for(int i = start ; i <= end; i++){
-    //         sum +=arr[i];
-    //     }
-    //     sum = sum * 2;
-    //     sum *= -1;
-    //     total = total + sum;
-    // }
-    // cout<<abs(total)<<endl;
-
+    cout<<maxi<<endl;
 }
 signed main(){
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
--- Problems-2/first.cpp
+++ Problems-2/first.cpp
@@ -18,22 +18,72 @@
 #define endl '\n'
 using namespace std;
 
-void solve() {   
-    int n, k; cin>>n>>k;
-    int arr[n]; for(int i = 0 ; i < n ; i++) cin>>arr[i];
-    sort(arr, arr + n);
-    int ans = 0;
-    int cnt =0;
-    for(int i = 1; i < n ; i++){
-        if(arr[i] - arr[i-1] <= k){
-            cnt++;
-            ans = max(cnt, ans);
-        }
-        else{
-            cnt = 0;
+vector<int> factors(int n){
+    vector<int> ans;
+    ans.push_back(1);
+    for(int i = 2; i * i <= n ; i++){
+        while(n % i == 0){
+            ans.push_back(i);
+            n /= i;
         }
     }
-    cout<<n - ans - 1<<endl;
+    if(n > 1) ans.push_back(n);
+    return ans;
+}
+
+
+int gcd(int a, int b){
+    vector<int> f1 = factors(a);
+    vector<int> f2 = factors(b);
+    // for(auto el : f1) cout<<el<<" ";
+    // cout<<endl;
+    // for(auto el : f2) cout<<el<<" ";
+    // cout<<endl;
+    unordered_map<int, int > um1, um2;
+    for(auto el : f1) um1[el]++;
+    for(auto el : f2) um2[el]++;
+    int ans = 1;
+    for(auto itr = um1.begin(); itr != um1.end(); itr++){
+        int pn = itr->first;
+        if(um2[itr->first] > 0){
+            int f = min(itr->second, um2[itr->first]);
+            while(f--){
+                ans *= (itr->first);
+            }
+        }
+    }
+    return ans;
+}
+
+void solve() {   
+    int n, q; cin>>n>>q;
+    int arr[n];
+    for(int i = 0 ; i < n ; i++) cin>>arr[i];
+    int sum = 0;
+    int pre[n];
+    pre[0] = arr[0];
+    for(int i = 1 ; i < n ; i++){
+        pre[i] = pre[i-1] + arr[i];
+    }
+    for(int i = 0 ; i < n ; i++) sum += arr[i];
+    while(q--){
+        int l, r, k ; 
+        cin>>l>>r>>k;
+        int tmp = k * (r - l + 1);
+        int s = 0;
+        if(l == 1){
+            s = pre[r-1];
+        }
+        else{
+            s = pre[r-1] - pre[l-2];
+        }
+        int v1 = sum - s;
+        int v2 = tmp;
+        int f = v1 + v2;
+        if(f & 1) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+    
 }
 signed main(){
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
