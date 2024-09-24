b07fe0f9ba63a9c1086eb38f304301eed798e218
sahilnarwal2111 <500097751@stu.upes.ac.in>
2024-09-24 12:29:05 +0530

    commit

--- Problems-2/first.cpp
+++ Problems-2/first.cpp
@@ -13,10 +13,11 @@
 #include <numeric>
 #include <climits>
 #define mod (ll)(1e9+7)
-#define int long long
+// #define int long long
 #define ll long long
 #define endl '\n'
 using namespace std;
+<<<<<<< HEAD
 /// cd "/Users/sahilnarwal/Documents/CP/Problems-2/" && g++ --std=c++20 dijsktra.cpp -o dijsktra && "/Users/sahilnarwal/Documents/CP/Problems-2/"dijsktra
 vector<int> factors(int n){
     vector<int> ans;
@@ -29,6 +30,35 @@ vector<int> factors(int n){
     }
     if(n > 1) ans.push_back(n);
     return ans;
+=======
+
+void solve() {   
+    int n, k;
+    cin>>n>>k;
+    string str; cin>>str;
+    int f[26];
+    for(int i = 0 ; i < 26 ; i++) f[i]  = 0;
+    for(int i = 0 ; i < n ; i++){
+        int idx = str[i] - 'a';
+        f[idx]++;
+    }
+    int odd = 0;
+    int even = 0;
+    for(int i = 0 ; i < 26 ; i++){
+        if(f[i] & 1) odd++;
+        else even++;
+    }
+    if(odd == 1) {
+        cout<<"YES"<<endl;
+        return ;
+    }
+    if(odd - 1 > k ){
+        cout<<"NO"<<endl;
+        return;
+    }
+    cout<<"YES"<<endl;
+
+>>>>>>> parent of 9ac6304 (day - 3)
 }
 
 
@@ -110,4 +140,117 @@ signed main(){
 }
 
 
+<<<<<<< HEAD
 
+=======
+    // int v1, v2;
+    // int ans = 0;
+    // // up a //left b
+    // v1 = y1 + a;
+    // v2 = x1 - b;
+    // //check
+    //     cout<<v1<<" "<<v2<<endl;
+    // if(v2 - b == x2 && v1 - a == y2) {
+    //     ans++;
+    // }
+
+    // //up a // right b
+    // v1 = y1 + a;
+    // v2 = x1 + b;
+    //     cout<<v1<<" "<<v2<<endl;
+    // if(v2 + b == x2 && v1 - a == y2) {
+    //     ans++;
+    // }
+
+    // // left a // up b
+    // v1 = y1 + b;
+    // v2 = x1 + a;
+    //     cout<<v1<<" "<<v2<<endl;
+    // if(v1 + b == y2 && v2 - a == x2) {
+    //     ans++;
+    // }
+
+    // //left a // down b
+    // v1 = y1 - b;
+    // v2 = x1 + a;
+    //     cout<<v1<<" "<<v2<<endl;
+    // if(v1 - b == y2 && v2 - a == x2) {
+    //     ans++;
+    // }
+
+    // // down a right b
+    // v1 = y1 - a;
+    // v2 = x1 + b;
+    //     cout<<v1<<" "<<v2<<endl;
+    // if(v2 + b == x2 && v1 +a == y2) {
+    //     ans++;
+    // }
+
+    // //down a left b
+    // v1 = y1 - a;
+    // v2 = x1 - b;
+    //     cout<<v1<<" "<<v2<<endl;
+    // if(v2 - b == x2 && v1 + a == y2) {
+    //     ans++;
+    // }
+
+    // //left a down b
+    // v1 = y1 - b;
+    // v2 = x1 - a;
+    //     cout<<v1<<" "<<v2<<endl;
+    // if(v1 - b == y2 && v2 + a == x2) {
+    //     ans++;
+    // }
+
+    // //left a up b
+    // v1 = y1 + b;
+    // v2 = x1 - a;
+    //     cout<<v1<<" "<<v2<<endl;
+    // if(v1 + b == y2 && v2 + a == x2) {
+    //     ans++;
+    // }
+
+
+    // //     // up a //left b
+    // // v1 = y1 + b;
+    // // v2 = x1 - a;
+    // // //check
+    // // if(v2 - a == x2 && v1 - b == y2) ans++;
+
+    // // //up a // right b
+    // // v1 = y1 + b;
+    // // v2 = x1 + a;
+    // // if(v2 + a == x2 && v1 - b == y2) ans++;
+
+    // // // left a // up b
+    // // v1 = y1 + a;
+    // // v2 = x1 + b;
+    // // if(v1 + a == y2 && v2 - b == x2) ans++;
+
+    // // //left a // down b
+    // // v1 = y1 - a;
+    // // v2 = x1 + b;
+    // // if(v1 - a == y2 && v2 - b == x2) ans++;
+
+    // // // down a right b
+    // // v1 = y1 - b;
+    // // v2 = x1 + a;
+    // // if(v2 + a == x2 && v1 +b == y2) ans++;
+
+    // // //down a left b
+    // // v1 = y1 - b;
+    // // v2 = x1 - a;
+    // // if(v2 - a == x2 && v1 + b == y2) ans++;
+
+    // // //left a down b
+    // // v1 = y1 - a;
+    // // v2 = x1 - b;
+    // // if(v1 - a == y2 && v2 + b == x2) ans++;
+
+    // // //left a up b
+    // // v1 = y1 + a;
+    // // v2 = x1 - b;
+    // // if(v1 + a == y2 && v2 + b == x2) ans++;
+
+    // cout<<ans<<endl;
+>>>>>>> parent of 9ac6304 (day - 3)
