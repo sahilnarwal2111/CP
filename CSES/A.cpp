445af264980239a4817f5e052ee4fc4fe1f6ac9f
sahilnarwal2111 <500097751@stu.upes.ac.in>
2024-10-11 21:37:20 +0530

    Movie Season - II | CSES Solutions

--- CSES/A.cpp
+++ CSES/A.cpp
@@ -20,47 +20,63 @@ using namespace std;
 void solve(){
     int n, k;
     cin >> n >> k;
-    string str;
-    cin >> str;
-
-    if(k >= 25){
-        for(int i = 0; i < n ; i++)
-            cout << 'a';
-        cout << '\n';
-        return;
-    }
-    int maxTillGreaterThanEqualToK = 0;
-    int firstGreaterThanK = -1;
-    for(int i = 0 ; i < n ; i++){
-        if(str[i] - 'a' > k){
-            firstGreaterThanK = i;
-            break;
-        }else{
-            maxTillGreaterThanEqualToK = max(maxTillGreaterThanEqualToK, (int)(str[i] - 'a'));
-        }
-    }
-    k = k - maxTillGreaterThanEqualToK;
-    int ref = str[firstGreaterThanK] - 'a' - k;
-    int tmp = str[firstGreaterThanK] - 'a';
-    for(int i = 0 ; i < n ; i++){
-        int idx = str[i] - 'a';
-        if(idx == 0)
-            continue;
-        if(idx <= maxTillGreaterThanEqualToK){
-            str[i] = 'a';
-        }else if(idx >= ref && idx <= tmp){
-            str[i] = (char) ref + 'a' ;
-        }
-    }
+    vector<pair<int, int>> arr(n);
+    for(auto &p : arr)
+        cin >> p.first >> p.second;
     
-    cout << str << '\n';
+    sort(arr.begin(), arr.end(), [](const pair<int,int> &p1, const pair<int,int> &p2){
+        if(p1.second < p2.second) return true;
+        if(p1.second == p2.second) return p1.first < p2.first;
+        return false;
+    });
+    multiset<int> st; // storing the ending of movie which ith person is watching
+    int cnt = 0;
+    for(int i = 0; i < n ; i++){
+        int start = arr[i].first;
+        //find suitable person who can watch this movie
+        if(st.size() < k) {
+            
+            if(st.size() == 0)
+                st.insert(arr[i].second);
+            else if(start >= *st.begin()){
+
+                auto itr = st.upper_bound(start);
+                if(itr != st.begin()){
+                    itr--;
+                }
+                st.erase(st.find(*itr));
+                st.insert(arr[i].second);
+
+            }else{
+                st.insert(arr[i].second);
+            }
+            cnt++;
+        }
+        else if(start >= *st.begin()){
+
+            auto itr = st.upper_bound(start);
+                if(itr != st.begin()){
+                    itr--;
+                }
+                st.erase(st.find(*itr));
+                st.insert(arr[i].second);
+
+
+            cnt++;
+            // cout << i << " b" << endl;
+            
+        }
+    }
+
+
+    cout << cnt << '\n';
     
 }    
 signed main(){
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);    
-    int tt;
-    cin >> tt;
-    while(tt--)
+    // int tt;
+    // cin >> tt;
+    // while(tt--)
         solve();
     return 0;
 }
---
+++ CSES/random.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cstdlib> // For rand() and srand()
+#include <ctime>   // For time()
+
+void generateRandomArray(int arr[], int n) {
+    for (int i = 0; i < n; ++i) {
+        arr[i] = rand() % 10 + 1; // Generates a random number between 1 and 10
+    }
+}
+
+int main() {
+    int n;
+    std::cout << "Enter the size of the arrays: ";
+    std::cin >> n;
+
+    // Seed the random number generator with current time
+    srand(time(0));
+
+    int arr1[n], arr2[n];
+
+    // Generate two random arrays
+    generateRandomArray(arr1, n);
+    generateRandomArray(arr2, n);
+
+    // Print the first array
+    std::cout << "Array 1: ";
+    for (int i = 0; i < n; ++i) {
+        std::cout << arr1[i] << " ";
+    }
+    std::cout << std::endl;
+
+    // Print the second array
+    std::cout << "Array 2: ";
+    for (int i = 0; i < n; ++i) {
+        std::cout << arr2[i] << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
