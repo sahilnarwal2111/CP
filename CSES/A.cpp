4133bf4aa9ac6a40f1ab084b3f78135572fc410c
sahilnarwal2111 <500097751@stu.upes.ac.in>
2024-10-13 11:40:38 +0530

    MIssing Coin Sum | CSES Solutions

--- CSES/A.cpp
+++ CSES/A.cpp
@@ -15,68 +15,56 @@
 #define int long long
 using namespace std;
 
-
-
 void solve(){
-    int n, k;
-    cin >> n >> k;
-    vector<pair<int, int>> arr(n);
-    for(auto &p : arr)
-        cin >> p.first >> p.second;
-    
-    sort(arr.begin(), arr.end(), [](const pair<int,int> &p1, const pair<int,int> &p2){
-        if(p1.second < p2.second) return true;
-        if(p1.second == p2.second) return p1.first < p2.first;
-        return false;
-    });
-    multiset<int> st; // storing the ending of movie which ith person is watching
-    int cnt = 0;
-    for(int i = 0; i < n ; i++){
-        int start = arr[i].first;
-        //find suitable person who can watch this movie
-        if(st.size() < k) {
-            
-            if(st.size() == 0)
-                st.insert(arr[i].second);
-            else if(start >= *st.begin()){
-
-                auto itr = st.upper_bound(start);
-                if(itr != st.begin()){
-                    itr--;
-                }
-                st.erase(st.find(*itr));
-                st.insert(arr[i].second);
-
-            }else{
-                st.insert(arr[i].second);
-            }
-            cnt++;
-        }
-        else if(start >= *st.begin()){
-
-            auto itr = st.upper_bound(start);
-                if(itr != st.begin()){
-                    itr--;
-                }
-                st.erase(st.find(*itr));
-                st.insert(arr[i].second);
-
-
-            cnt++;
-            // cout << i << " b" << endl;
-            
-        }
+    int n, c, d;
+    cin >> n >> c >> d;
+    // c -> cost of removing
+    // d -> cost of insertion
+    vector<int> arr(n);
+    for(auto &i : arr)
+        cin >> i;
+    sort(arr.begin(), arr.end());
+    vector<int> brr;
+    int costOfInsertions = 0;
+    if(arr[0] != 1){
+        brr.push_back(1);
+        costOfInsertions += d;
     }
+    brr.push_back(arr[0]);
+    for(auto &i : arr)
+        if(brr.back() != i)
+            brr.push_back(i);
+    int duplicates = arr.size() - brr.size();
+    if(arr[0] != 1){
+        duplicates++;
+    }
+    int ans = LLONG_MAX;
+    int N = brr.size();
+    for(int i = 1; i < N ; i++){
+        // we consider to remove all brr starting from index i 
+        int elementsToBeRemoved = N - i;
+        // total cost = cost of inserting till now and removing the elment left right of i
+        int cost = elementsToBeRemoved * c + costOfInsertions;
+        ans = min(ans, cost);
+        // we consider to add all the missing numbers
+        int missing = brr[i] - brr[i-1] - 1;
+        costOfInsertions += (missing * d);
 
-
-    cout << cnt << '\n';
+    }
+    // we reached the end of array and if we want to keep all the numbers and dont wanna to remove any one
+    ans = min(ans, costOfInsertions);
     
+    
+    // also have a look at the case of size equals to largest element
+    
+    //duplicates removel cost need to be added in last
+    cout << ans + duplicates * c << '\n';
 }    
 signed main(){
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);    
-    // int tt;
-    // cin >> tt;
-    // while(tt--)
+    int tt;
+    cin >> tt;
+    while(tt--)
         solve();
     return 0;
 }
